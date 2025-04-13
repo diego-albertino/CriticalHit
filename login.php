2b42da1fcdd0f34de6adeadedd8a086ff6c5b83e
Felipe Miranda <felipeszm_dev@hotmail.com>
2025-04-13 12:09:16 -0400

    Criado um favicon da Criticalhit

--- login.php
+++ login.php
@@ -16,6 +16,7 @@ if (isset($_GET['contaJaCriada'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="icon" href="pictures/favicon.png" type="image/x-icon" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="login.css">
