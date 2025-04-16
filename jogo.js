d4caa32e66b0ff48d2840ab6086c5ebcb56e7b20
Diego <diegoalbertinodasilvarodrigues@gmail.com>
2025-04-16 12:39:24 -0400

    Adicionado o botão pra fazer comentário no arquivo jogo.html e realizado a estilização do botão no jogo.css

--- jogo.js
+++ jogo.js
@@ -1,53 +1,54 @@
-// Dados dos jogos
+var num_star = 5; // Variável para definir o número de estrelas
+
 const games = {
   "stardew-valley": {
     title: "Stardew Valley",
     image: "pictures/stardew.png",
     description: "Você herdou a antiga fazenda do seu avô, em Stardew Valley. Com ferramentas de segunda-mão e algumas moedas, você parte para dar início a sua nova vida. Será que você vai aprender a viver da terra, a transformar esse matagal em um próspero lar?",
-    rating: "★★★★★",
+    rating: "★".repeat(num_star), // repeat repete a string "★" de acordo com o valor de num_star
   },
   "nfs-heat": {
     title: "Need For Speed Heat",
     image: "pictures/nfs.png",
     description: "Trabalhe de dia e arrisque tudo à noite em Need for Speed™ Heat, um jogo eletrizante de corridas de rua, onde a lei desaparece com o pôr do sol.",
-    rating: "★★",
+    rating: "★".repeat(num_star), 
   },
   "silent-hill-2": {
     title: "Silent Hill 2",
     image: "pictures/silent.png",
     description: "Um clássico do terror psicológico.",
-    rating: "★★★★",
+    rating: "★".repeat(num_star),
   },
   "spider-man-2": {
     title: "Spider-Man 2",
     image: "pictures/spider.png",
     description: "Aventura épica com o Homem-Aranha.",
-    rating: "★★★",
+    rating: "★".repeat(num_star),
   },
 
   "elden-ring-nightreign": {
     title: "Elden Ring Nightreign",
     image: "pictures/elden.png",
     description: "Uma jornada épica em um mundo aberto.",
-    rating: "★★★★★",
+    rating: "★".repeat(num_star),
   },
   "death-stranding-2": {
     title: "Death Stranding 2",
     image: "pictures/death.png",
     description: "Uma experiência única de entrega e conexão.",
-    rating: "★★★★",
+    rating: "★".repeat(num_star),
   },
   "call-of-duty": {
     title: "Call of Duty",
     image: "pictures/cod.png",
     description: "Um dos maiores jogos de tiro em primeira pessoa.",
-    rating: "★★★",
+    rating: "★".repeat(num_star),
   },
   "deliver-at-all-costs": {
     title: "Deliver At All Costs",
     image: "pictures/deliver.png",
     description: "Um jogo de entrega em um mundo pós-apocalíptico.",
-    rating: "★★★",
+    rating: "★".repeat(num_star),
   },
 };
 
@@ -64,15 +65,18 @@ function loadGameDetails() {
 
     // Renderizar os detalhes do jogo
     gameDetails.innerHTML = `
-    <div style="display: flex; align-items: flex-start; gap: 90px">
-        <img src="${game.image}" alt="${game.title}" style="height: 424px; width: 624px;" />
-        <div style="display: flex; flex-direction: column;">
-            <h1>${game.title}</h1>
-            <p>${game.description}</p>
-            <div class="rating"> ${game.rating}</div>
+    <div class="game-container">
+        <img src="${game.image}" alt="${game.title}" class="game-image" />
+        <div class="game-info">
+            <h1 class="game-title">${game.title}</h1>
+            <p class="game-description">${game.description}</p>
+            <div class="rating">${game.rating}</div>
         </div>
     </div>
-  `;
+    <div class="comment-container">
+        <button type="submit" class="btn btn-comentar btn-lg">Comentar</button>
+    </div>
+    `;
   } else {
     // Caso o jogo não seja encontrado
     document.getElementById("game-details").innerHTML = `
