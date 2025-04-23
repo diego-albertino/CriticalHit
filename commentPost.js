0ea45dabc49cb3027a73f51997c84d49ea151f45
Diego <diegoalbertinodasilvarodrigues@gmail.com>
2025-04-23 11:09:06 -0400

    Layout de visualização de comentários aprimorado

--- commentPost.js
+++ commentPost.js
@@ -6,8 +6,8 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let p = document.createElement("p");
-  p.classList = "p-2 d-flex text-wrap";
-  p.innerHTML = `<strong>Usuário: </strong>&nbsp ${textComment.value}&nbsp <br><strong>Avaliação: </strong>&nbsp ${localStorage.getItem("selectedRating")}`;
+  p.classList = "p-2 text-wrap";
+  p.innerHTML = `<i class="bi bi-person-circle">&nbsp </i><strong>Usuário: </strong>&nbsp ${textComment.value}&nbsp <br> <span class="star-commentPost">${"★".repeat(localStorage.getItem("selectedRating"))}</span>`;
   commentPost.appendChild(p);
   textComment.value = ""; // Limpa o campo de comentário após o envio
 });
