5f5f7a3b37b0d8a3dddaecb54059fa69154ca2f5
Diego <diegoalbertinodasilvarodrigues@gmail.com>
2025-04-23 13:32:05 -0400

    Adicionado a divisão entre os comentários e melhorado a exibição do texto do comentário.

--- commentPost.js
+++ commentPost.js
@@ -6,8 +6,9 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let p = document.createElement("p");
-  p.classList = "p-2 text-wrap";
-  p.innerHTML = `<i class="bi bi-person-circle">&nbsp </i><strong>Usuário: </strong>&nbsp ${textComment.value}&nbsp <br> <span class="star-commentPost">${"★".repeat(localStorage.getItem("selectedRating"))}</span>`;
+  p.classList = "rounded-2 bg-body-secondary p-3 w-100 mt-4 text-wrap";
+  p.innerHTML = `<div class="comment-paragraph"><i class="bi bi-person-circle">&nbsp </i><strong>Usuário: </strong><br> ${textComment.value}<br><span class="star-commentPost">${"★".repeat(localStorage.getItem("selectedRating"))}</span></div>`;
   commentPost.appendChild(p);
   textComment.value = ""; // Limpa o campo de comentário após o envio
+  localStorage.removeItem("selectedRating"); // Remove a classificação após o envio do comentário
 });
